bec0cab66ca429f35b3c113665e68bd2b47b78c3
SWAlbert <1261920970@qq.com>
2025-04-18 13:57:30 +0800

    Initial commit

---
+++ user-service/src/main/java/com/eduonline/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eduonline;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserServiceApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
